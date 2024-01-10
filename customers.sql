79124a086263eb8b5c5b670d170ee468304a4ef8
April Williford <34241214+avianaw@users.noreply.github.com>
2024-01-10 22:16:35 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,12 @@
+select
+c.id,
+c.name,
+c.email,
+min(o.created_at) as first_order_at,
+count(o.customer_id) as number_of_orders
+from  `analytics-engineers-club.coffee_shop.customers` c
+join `analytics-engineers-club.coffee_shop.orders` o
+on c.id = o.customer_id
+group by c.id,c.name,c.email
+order by first_order_at
+limit 5;
